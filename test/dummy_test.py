45dc045d2d28fcc57c1726f6c9d7535ca5e2211b
Julien Férard <julien_ferard@yahoo.fr>
2018-03-07 16:56:42 +0100

    Add travis CI Add a dummy test

---
+++ test/dummy_test.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""sqliteondbf - SQLite on DBF
+      Copyright (C) 2018 J. Férard <https://github.com/jferard>
+   This file is part of sqliteondbf.
+   sqliteondbf is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   sqliteondbf is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   """
+import unittest
+
+class Dummy(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
